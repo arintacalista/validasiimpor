501136983bc9f29ea3b53bc431cb90f995d91389
Jovi <redzjovi@gmail.com>
2017-08-05 12:58:27 +0700

    Update line chart in home.

--- protected/views/site/home.php
+++ protected/views/site/home.php
@@ -1,18 +1,3 @@
-<head>
-
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
-
-
-<!-- <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div> -->
-
-</head>
-
-<?php
-
-
-$this->pageTitle=Yii::app()->name;
-?>
 <style type="text/css">
    .left    { text-align: left;}
    .right   { text-align: right;}
@@ -20,31 +5,45 @@ $this->pageTitle=Yii::app()->name;
    .justify { text-align: justify;}
 </style>
 <br>
+
+<?php $this->pageTitle = Yii::app()->name; ?>
 <h1 class="center">Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-<br>
-<br>
-<?php
-$hs14 = Hs14::model()->find([
-    'select' => 'SUM(CIFKG) AS TOTAL, WAKTU',
+<br />
+
+<?php $hs14 = Hs14::model()->findAll([
+    'select' => ['idhs14', 'SUM(CIFKG) AS CIFKG', 'WAKTU'],
     'condition' => 'WAKTU >= :waktu_dari AND WAKTU <= :waktu_sampai',
     'group' => 'WAKTU',
     'params' => [':waktu_dari' => '201501', ':waktu_sampai' => '201512']
 ]);
-dump($hs14);
+// dump($hs14);
+
+$data = CHtml::listData($hs14, 'idhs14' , 'WAKTU');
+$categories = array_values($data);
+// dump($categories);
+
+$data = CHtml::listData($hs14, 'idhs14' , 'CIFKG');
+$data = array_values($data);
+$data = array_map(
+    function ($value) { return  floatval($value); },
+    $data
+);
+// dump($data);
 ?>
+
 <?php $this->Widget('ext.highcharts.HighchartsWidget', array(
-   'options' => array(
-      'title' => array('text' => 'Fruit Consumption'),
-      'xAxis' => array(
-         'categories' => array('Apples', 'Bananas', 'Oranges')
-      ),
-      'yAxis' => array(
-         'title' => array('text' => 'Fruit eaten')
-      ),
-      'series' => array(
-         array('name' => 'Jane', 'data' => array(1, 0, 4)),
-         array('name' => 'John', 'data' => array(5, 7, 3))
-      )
-   )
-));
-?>
+    'options' => array(
+        'title' => array('text' => 'Fruit Consumption'),
+        'xAxis' => array(
+            // 'categories' => array('Apples', 'Bananas', 'Oranges'),
+            'categories' => $categories,
+        ),
+        'yAxis' => array(
+            'title' => array('text' => 'Fruit eaten')
+        ),
+        'series' => array(
+            // array('name' => 'Jane', 'data' => array(1, 0, 4)),
+            ['name' => 'total', 'data' => $data],
+        )
+    )
+)); ?>
